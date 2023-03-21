a48d6333837e4275c65359e253918f2848293e11
Logan <2727646e@student.gla.ac>
2023-03-21 16:19:23 +0000

    Finished_Test

--- project/rango/models.py
+++ project/rango/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Page(models.Model):
+    title = models.TextField()
+    author = models.TextField()
+    date = models.TextField()
+    news = models.TextField()
+    def __str__(self):
+        return self.title
--- project/rango/views.py
+++ project/rango/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Page
 def index(request):
-
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    list=Page.objects.all()
+    context_dict = {'news': list}
 
     return render(request, 'rango/index.html', context=context_dict)
 
